cfa9620ede63f81c0f5d4ce6fed11d4d2b179283
POO <y.huang01@ufromail.cl>
2026-04-14 19:55:11 -0400

    un avance de ejecutarOpcion la clase menu

--- src/Menu.java
+++ src/Menu.java
@@ -15,6 +15,28 @@ public class Menu {
         int opcionSelecionada = scanner.nextInt();
         return opcionSelecionada;
     }
+    public static void ejecutarOpcion(int menuOpcion, Scanner scanner) {
+        switch(menuOpcion) {
+            case 1:
+                /**OperacionesAritmeticas
+                break;
+            case 2:
+                EcuacionCuadratica
+                break;
+            case 3:
+                FigurasGeometricas
+                break;
+            case 4:
+                SistemaEcuacionesLineales
+                break;
+            case 5:
+                EcuacionRecta
+                break;**/
+            case 6:
+                break;
+        }
+
+    }
 
 
 
