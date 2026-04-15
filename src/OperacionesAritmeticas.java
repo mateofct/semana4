3ff6aaeb30b785bc2aea4d166764037c89d4a0eb
POO <y.huang01@ufromail.cl>
2026-04-15 14:00:21 -0400

    nueva estructura para la resta

--- src/OperacionesAritmeticas.java
+++ src/OperacionesAritmeticas.java
@@ -89,15 +89,28 @@ public class OperacionesAritmeticas {
         double resultadoSuma = calcularSuma(primerNumero, segundoNumero);
         mostrarResultadoSuma(resultadoSuma);
     }
-
-    public static void resta(Scanner scanner) {
+    public static double[] pedirDatosResta(Scanner scanner){
         System.out.println("Ingrese el numero que va adelante de la resta:");
         double primerNumero = scanner.nextDouble();
         System.out.println("Ingrese el segundo número:");
         double segundoNumero = scanner.nextDouble();
-        double resultadoresta = primerNumero - segundoNumero;
-        System.out.println("El resultado de la resta es: " + resultadoresta);
+        return new double[]{primerNumero, segundoNumero};
     }
+    public static double calcularResta(double primerNumero, double segundoNumero){
+        double resultadoResta = primerNumero - segundoNumero;
+        return resultadoResta;
+    }
+    public static void mostrarResultadoResta(double resultadoResta){
+        System.out.println("El resultado de la resta es: " + resultadoResta);
+    }
+    public static void ejecutarResta(Scanner scanner){
+        double[] datosResta = pedirDatosResta(scanner);
+        double primerNumero = datosResta[0];
+        double segundoNumero = datosResta[1];
+        double resultadoResta = calcularResta(primerNumero, segundoNumero);
+        mostrarResultadoResta(resultadoResta);
+    }
+    
 
     public static void multiplicacion(Scanner scanner) {
         System.out.println("Ingrese el primer número:");
