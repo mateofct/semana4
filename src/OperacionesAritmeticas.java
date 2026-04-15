f5258a9d633da22aa50366981d79a80d485f31df
POO <y.huang01@ufromail.cl>
2026-04-14 23:34:15 -0400

    metodo de potencia

--- src/OperacionesAritmeticas.java
+++ src/OperacionesAritmeticas.java
@@ -124,4 +124,14 @@ public class OperacionesAritmeticas {
             System.out.println("El número menor es: " + resultadoNumeroMenor);
         }
     }
+    public static void potencia(Scanner scanner){
+        System.out.println("Ingrese el valor del número base:");
+        double numeroBase = scanner.nextDouble();
+        System.out.println("Ingrese el valor del número exponente:");
+        double numeroExponente = scanner.nextDouble();
+        double resultadoPotencia = Math.pow(numeroBase, numeroExponente);
+        System.out.println("El resultado de la potencia es: " + resultadoPotencia);
+
+    }
+
 
