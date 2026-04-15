73ecd6f3383c4556d5328380758590dc68d12eda
POO <y.huang01@ufromail.cl>
2026-04-15 14:24:14 -0400

    Nueva clase Resta

--- src/OperacionesAritmeticas.java
+++ src/OperacionesAritmeticas.java
@@ -40,7 +40,8 @@ public class OperacionesAritmeticas {
                 suma.ejecutarSuma(scanner);
                 break;
             case 2:
-                ejecutarResta(scanner);
+                Resta resta = new Resta();
+                resta.ejecutarResta(scanner);
                 break;
             case 3:
                 ejecutarMultiplicacion(scanner);
@@ -70,27 +71,7 @@ public class OperacionesAritmeticas {
     }
 
 
-    public static double[] pedirDatosResta(Scanner scanner){
-        System.out.println("Ingrese el numero que va adelante de la resta:");
-        double primerNumero = scanner.nextDouble();
-        System.out.println("Ingrese el segundo número:");
-        double segundoNumero = scanner.nextDouble();
-        return new double[]{primerNumero, segundoNumero};
-    }
-    public static double calcularResta(double primerNumero, double segundoNumero){
-        double resultadoResta = primerNumero - segundoNumero;
-        return resultadoResta;
-    }
-    public static void mostrarResultadoResta(double resultadoResta){
-        System.out.println("El resultado de la resta es: " + resultadoResta);
-    }
-    public static void ejecutarResta(Scanner scanner){
-        double[] datosResta = pedirDatosResta(scanner);
-        double primerNumero = datosResta[0];
-        double segundoNumero = datosResta[1];
-        double resultadoResta = calcularResta(primerNumero, segundoNumero);
-        mostrarResultadoResta(resultadoResta);
-    }
+
     public static double[] pedirDatosMultipliacion(Scanner scanner){
         System.out.println("Ingrese el primer número:");
         double primerNumero = scanner.nextDouble();
