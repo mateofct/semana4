8d349f9aff235e58aa73af211ebf58091cfaac6f
POO <y.huang01@ufromail.cl>
2026-04-15 00:10:05 -0400

    metodo para pedirle los datos al usuario

--- src/EcuacionCuadratica.java
+++ src/EcuacionCuadratica.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
 public class EcuacionCuadratica {
+    public static void pedirDatos(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("La ecuación cuadrática tiene la forma ax^2 + bx + c = 0");
+        System.out.println("Ingrese el valor de a:");
+        double a = scanner.nextDouble();
+        System.out.println("Ingrese el valor de b:");
+        double b = scanner.nextDouble();
+        System.out.println("Ingrese el valor de c:");
+        double c = scanner.nextDouble();
+
+
+    }
+
 }
