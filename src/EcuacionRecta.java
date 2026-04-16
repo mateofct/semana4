149f325b0a1828620dcf094e8adfb4ef7e0c69f0
mateo <fct.matteo@gmail.com>
2026-04-15 23:32:01 -0400

    uso de try catch por si el usuario ingresa una entrada no válida

--- src/EcuacionRecta.java
+++ src/EcuacionRecta.java
@@ -1,32 +1,39 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class EcuacionRecta {
 
     public static void ejecutar(Scanner scanner) {
         System.out.println("Calcular ecuación de la recta con 2 puntos.");
-        System.out.print("Ingrese x1: ");
-        double x1 = scanner.nextDouble();
-        System.out.print("Ingrese y1: ");
-        double y1 = scanner.nextDouble();
-        System.out.print("Ingrese x2: ");
-        double x2 = scanner.nextDouble();
-        System.out.print("Ingrese y2: ");
-        double y2 = scanner.nextDouble();
 
-        if (x1 == x2) {
-            System.out.println("Error, pendiente indefinida");
-        } else {
-            double m = calcularPendiente(x1, y1, x2, y2);
-            double b = calcularInterseccion(x1, y1, m);
+        try {
+            System.out.print("Ingrese x1: ");
+            double x1 = scanner.nextDouble();
+            System.out.print("Ingrese y1: ");
+            double y1 = scanner.nextDouble();
+            System.out.print("Ingrese x2: ");
+            double x2 = scanner.nextDouble();
+            System.out.print("Ingrese y2: ");
+            double y2 = scanner.nextDouble();
 
-            if (b >= 0) {
-                System.out.println("La ecuación es:");
-                System.out.println("Y = " + m + "X + " + b);
+            if (x1 == x2) {
+                System.out.println("Error, pendiente indefinida");
             } else {
-                // b negativo
-                System.out.println("La ecuación es:");
-                System.out.println("Y = " + m + "X " + b);
+                double m = calcularPendiente(x1, y1, x2, y2);
+                double b = calcularInterseccion(x1, y1, m);
+
+                if (b >= 0) {
+                    System.out.println("La ecuación es:");
+                    System.out.println("Y = " + m + "X + " + b);
+                } else {
+                    // b negativo
+                    System.out.println("La ecuación es:");
+                    System.out.println("Y = " + m + "X " + b);
+                }
             }
+        } catch (InputMismatchException e) {
+            System.out.println("Error, entrada inválida");
+            scanner.nextLine();
         }
     }
 
